71707dcee3903dfc947a8dda841f582a518f8a33
Mathilde Calon <mathilde.calon@gmail.com>
2023-12-04 15:10:14 +0100

    construction

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <div className='main'> 
+      <h1>Site en construction ! Merci de visiter notre <a href='https://www.facebook.com/profile.php?id=61552552079121'>page Facebook</a></h1>
+      <img src='/logo.jpg'></img>
+    </div>
+    </>
+  )
+}
+
+export default App
