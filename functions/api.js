85aa4ff86ac211b9b757ac600843eb2683c3049d
Mathilde Calon <mathilde.calon@gmail.com>
2024-02-09 11:54:26 +0100

    connexion to DB OK

--- functions/api.js
+++ functions/api.js
@@ -1,21 +1,25 @@
-import express, { Router } from 'express';
+import express from 'express';
 import cors from 'cors';
+import pg from 'pg';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const { Client } = pg;
 const app = express();
-const router = new Router();
+const router = express.Router();
 
-const PORT = 3000;
+const client = new Client(process.env.DATABASE_URL)
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // === CORS === //
+// AUTORISE LA COMMUNICATION ENTRE LE FRONT ET LE BACK //
 app.use(
     cors({
-      origin: [
-        // === LOCAL DEV === //
-        'http://localhost:5173',
-      ],
+      origin: 'http://localhost:5173',
       credentials: true,
       methods: 'GET,PATCH,POST,DELETE',
     })
@@ -23,16 +27,38 @@ app.use(
 
 app.use(router);
 
-router.post("/newsletter", (req, res)=>{
-    console.log("connexion OK");
-    const email = req.body.newsletter;
-    const emails = [];
-    console.log(`avant le push` + emails);
-    emails.push(email);
-    console.log(`après le push` + emails);
+// === INSCRIPTION A LA NEWSLETTER === //
+router.post("/newsletter", 
+    async (req, res)=>{
+        try {
+            // RECUPERATION DE L'ADRESSE MAIL
+            const email = req.body.newsletter;
+
+            // CONNEXION A LA BDD
+            await client.connect();
+
+            // INSERTION EN BDD
+            const insertQuery = {
+                text: `INSERT INTO "newsletter"(email, subscribed) VALUES ($1, 'true')`,
+                values: [email]
+            }
+            const response = await client.query(insertQuery);
+
+            if(!response.rowCount){
+                console.log(`Erreur lors de l'inscription`)
+            } else {
+                console.log("Inscription réussie.")
+            }
+            // FERMETURE DE LA CONNEXION
+            await client.end();
+        } catch(error) {
+            console.error(error);
+        };
+        res.redirect('https://ladc-evenements.fr/');
     }
 );
 
+// === LANCEMENT DU SERVEUR === //
 app.listen(PORT, () => {
-    console.log(`Server is running`)
+    console.log(`Server is running on port ${PORT}`);
 })
