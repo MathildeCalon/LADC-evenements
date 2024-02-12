c7a04a91d426c62658329054470aced8db3185e3
Mathilde Calon <mathilde.calon@gmail.com>
2024-02-12 16:59:17 +0100

    popup display position fixed

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -6,9 +6,9 @@ import logo from '/images/logo-banner.png';
 const Popup = props => {
 return (
     <div className={styles.popupBox}>
-        <div className={styles.box}>
-        <i className={`fa-regular fa-circle-xmark ${styles.btnClose}`} onClick={props.handleClose}></i>
-        <div className='d-flex flex-column justify-content-center align-items-center p-20'>
+        <div className={`${styles.box}`}>
+            <i className={`fa-regular fa-circle-xmark ${styles.btnClose}`} onClick={props.handleClose}></i>
+            <div className='d-flex flex-column justify-content-center align-items-center p-20'>
                 <img src={logo} alt="Logo de LADC Evenements"/>
                 <h3 className='m-10'>Abonnez-vous à notre newsletter et ne manquez aucune information :</h3>
                 <div className={styles.newsletterForm}>
