ca21e7bd7827458dd231e288eadfb9e23fbaa4ef
Mathilde Calon <mathilde.calon@gmail.com>
2024-02-28 16:48:08 +0100

    add to cart button css OK

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -5,18 +5,22 @@ import jsonData from '../assets/articles.json';
 export default function Articles ({addToCart}){
     return(
         jsonData.map((article) => (
-            <div className={styles.article} key={article.id}>
+            <div className={`${styles.article} d-flex flex-column align-items-center`} key={article.id}>
                 {article.photo &&
                 <div className={`${styles.imageContainer} d-flex`}>
                     <img src={`/images/${article.photo}`} alt={article.titre} className='m-20'/>
                 </div>}
                 <div className={`${styles.articleTitle} d-flex flex-column justify-content-center align-items-center`}>
-                    <h3>{article.titre} {article.prix}€</h3>
-                    <i className="fa-solid fa-cart-plus" onClick={()=>{addToCart(article)}}></i>
+                    <h3>{article.titre}</h3>
                     {article.description &&
                         <p className={styles.description}>{article.description}</p>
                     }
                 </div>
+                <div className={`${styles.addCart} d-flex align-items-center justify-content-center py-10 my-15`} onClick={()=>{addToCart(article)}}>
+                    <i className='fa-solid fa-cart-plus mr-15'/>
+                    <p>Ajouter au panier - {article.prix}€</p>  
+                </div>
+
             </div>
         ))
     )
