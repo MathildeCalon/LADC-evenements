67f9d287735c89a21fa0cf7f8581bf85e6106c77
Mathilde Calon <mathilde.calon@gmail.com>
2024-02-28 11:54:59 +0100

    css improved in cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -11,28 +11,32 @@ const Cart = ({currentCart, handleClose, removeFromCart}) => {
                 <i className={`fa-regular fa-circle-xmark ${styles.btnClose}`} onClick={handleClose}></i>
                 <div className={`d-flex flex-column justify-content-center align-items-center p-20`}>
                     <img src={logo} alt="Logo de LADC Evenements"/>
-                    <label for="start">Date de réservation souhaitée :</label>
-                    <input type="date" id="start" name="booking-start" className={styles.startDate}/>
+                    <div className='d-flex my-15'>
+                        <label for="start">Date de réservation souhaitée :</label>
+                        <input type="date" id="start" name="booking-start" className={styles.startDate}/>
+                    </div>
                     {currentCart.map((article) => (
                         <div className={styles.article} key={article.id}>
+                            <div className={`${styles.articleResume} d-flex align-items-center`}>
                             {article.photo &&
-                            <div className={`${styles.imageContainer} d-flex`}>
-                                <img src={`/images/${article.photo}`} alt={article.titre} className='m-20'/>
-                            </div>}
-                            <div className={`${styles.articleTitle} d-flex flex-column justify-content-center align-items-center`}>
-                                <h3>{article.titre} {article.prix}€</h3>
-                                <i className="fa-solid fa-trash-can" onClick={()=>{removeFromCart(article)}}></i>
-                                {article.description &&
-                                    <p className={styles.description}>{()=>(article.description)}</p>
-                                }
+                                <div className={`${styles.imageContainer} d-flex`}>
+                                    <img src={`/images/${article.photo}`} alt={article.titre} className='m-20'/>
+                                </div>}
+                                <div className={`${styles.articleTitle} d-flex flex-column justify-content-center align-items-center`}>
+                                    <h3>{article.titre}</h3>
+                                    <p>{article.prix}€</p>
+                                </div>
+                                <i className="fa-solid fa-trash-can mr-15" onClick={()=>{removeFromCart(article)}}></i>
                             </div>
                         </div>
                     ))}
                     <p className={styles.totalSum}>Montal total : {totalSum}€</p>
                     {currentCart.length === 0 && (
-                    <p>Aucun article dans votre panier.</p>)}
+                    <p className={`${styles.noArticle} p-20`}>Aucun article dans votre panier.</p>)}
 
-                    <button> AJOUTER BOUTON D'ENVOI </button>
+                    {currentCart.length > 0 && ( 
+                    <button className='btn btn-primary btn-big'>Envoyez votre demande de réservation !</button>
+                    )}
                 </div>
             </div>
         </div>
