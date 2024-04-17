3108489e3c6d0af0635886bd69f2417829356ff3
Mathilde Calon <mathilde.calon@gmail.com>
2024-04-17 11:35:57 +0200

    pictures and addtocart button highlighted on hover

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,13 @@ function App() {
   // AJOUT AU PANIER
   const [currentCart, setCurrentCart] = useState([]);
   const addToCart = (article) => {
-    setCurrentCart([...currentCart, article]);
+    // VERIFICATION DE LA PRESENCE DE L'ARTICLE DANS LE PANIER
+    const check = currentCart.find((el) => el.id === article.id);
+    if (check) {
+      return;
+    } else {
+      setCurrentCart([...currentCart, article]);
+    };
 
     // CONFIRMATION DE L'AJOUT AU PANIER
     const clickedBtn = document.getElementById(`btn${article.id}`);
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -63,7 +63,7 @@ function Header ({currentCart, removeFromCart, togglePopup, newsIsOpen}){
                         Contact
                         </button>
                     </a>
-                    <button className={`btn btn-primary mr-15`} onClick={toggleCartPopUp}><i className="fa-solid fa-cart-shopping mr-5"/>Mon panier - 
+                    <button className={`btn btn-primary mr-15`} onClick={toggleCartPopUp}><i className="fa-solid fa-cart-shopping mr-5"/>Mon panier 
                     <span className={`${styles.cartLength} justify-content-center align-items-center ml-5`}>{currentCart.length}</span></button>
                 </ul>
                 {cartIsOpen && <Cart
