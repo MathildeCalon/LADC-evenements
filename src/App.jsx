482c4a175171c70a12bb02ceede872fb92a88a40
Mathilde Calon <mathilde.calon@gmail.com>
2024-05-02 12:42:51 +0200

    galeriePhotos started, needs pictures

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import styles from './App.module.scss';
 
 import React from 'react';
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './components/mainComponents/MainPage.jsx';
 import LivredOr from './components/mainComponents/LivredOr.jsx';
@@ -10,9 +11,9 @@ function App() {
   return (
     <Router>
       <Routes>
+      <Route exact path="/" Component={MainPage} />
         <Route exact path="/livredor" Component={LivredOr} />
         <Route exact path="/galeriephotos" Component={GaleriePhotos} />
-        <Route exact path="/" Component={MainPage} />
       </Routes>
     </Router>
   )
--- src/components/mainComponents/GaleriePhotos.jsx
+++ src/components/mainComponents/GaleriePhotos.jsx
@@ -1,8 +1,26 @@
+import styles from './GaleriePhotos.module.scss';
+
 const GaleriePhotos = () => {
     return (
-        <>
-        <span>Galerie Photos</span>
-        </>
+        <div className={`d-flex flex-column ${styles.appContainer}`}>
+            <header className={`${styles.header} d-flex flex-row align-items-center`}>
+                <div className="flex-fill">
+                    <a href='/'>
+                    <h1>LADC Evénements</h1>
+                    </a>
+                </div>
+
+                <nav>
+                    <a href='/'>
+                    <button className={`btn btn-primary mr-15`}>Accueil</button>
+                    </a>
+                </nav>
+            </header>
+
+            <div className='flex-fill container p-20'>
+                <h2>Galerie Photos</h2>
+            </div>
+        </div>
     )
 }
 
