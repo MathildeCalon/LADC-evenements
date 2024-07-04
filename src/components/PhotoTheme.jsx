d55cef4ad3fbbd667a06df5c5581e04e5c83fe18
Mathilde Calon <mathilde.calon@labom2iformation.fr>
2024-07-04 14:45:29 +0200

    galerie continued, css better but still needs to be fixed

--- src/components/PhotoTheme.jsx
+++ src/components/PhotoTheme.jsx
@@ -1,4 +1,5 @@
 import galerie from '../assets/galerie.json';
+import styles from './PhotoTheme.module.scss';
 
 const PhotoTheme = ({theme}) => {
     const themes = galerie.filter(el => el.theme === theme);
@@ -6,12 +7,14 @@ const PhotoTheme = ({theme}) => {
     return (
         <div>
             <h3>{theme}</h3>
+            <div className="d-flex flex-row">
             {themes.map(item => (
-                <div key={item.id}>
-                    <img src={`/images/galerie/${item.url}`} alt={item.description} />
+                <div key={item.id} >
+                    <img src={`/images/galerie/${item.url}`} alt={item.description} className={styles.picGalery}/>
                     <p>{item.description}</p>
                 </div>
             ))}
+            </div>
         </div>
     )
 }
